394dd81f8c40c530d699fdbded27169e839cca74
DenisMedko <medko.denis76@gmail.com>
2023-02-10 14:23:04 +0200

    server env and configs changed

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ const tokenErrorHandler = require('./handlerError/tokenHandler');
 const multerErrorHandler = require('./handlerError/multerHandler');
 const { FILES_PATH } = require('./constants');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
