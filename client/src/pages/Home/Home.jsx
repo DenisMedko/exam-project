ce2cf34d610300bc78a1671643287e2646ad3546
DenisMedko <medko.denis76@gmail.com>
2023-02-19 23:09:22 +0200

    HomePage refactor

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -8,6 +8,7 @@ import styles from './Home.module.sass';
 import carouselConstants from '../../carouselConstants';
 import Spinner from '../../components/Spinner/Spinner';
 import HeadlineText from './HeadlineText.jsx';
+import mocks from '../../utils/mocks/homePageMocks';
 
 const Home = (props) => {
   const { isFetching, role } = props;
@@ -15,22 +16,16 @@ const Home = (props) => {
   return (
     <>
       <Header />
-      {isFetching ? (
-        <Spinner />
-      ) : (
+      {isFetching && <Spinner />}
+      {!isFetching && (
         <>
           <div className={styles.container}>
             <div className={styles.headerBar}>
               <div className={styles.headline}>
-                <span>Find the Perfect Name for</span>
+                <span>{mocks.header.title}</span>
                 <HeadlineText />
               </div>
-              <p>
-                Launch a naming contest to engage hundreds of naming experts as
-                you’re guided through our agency-level naming process. Or,
-                explore our hand-picked collection of premium names available
-                for immediate purchase
-              </p>
+              <p>{mocks.header.text}</p>
               {role && (
                 <div className={styles.button}>
                   <Link className={styles.button__link} to="/dashboard">
@@ -53,41 +48,24 @@ const Home = (props) => {
                     src={`${CONSTANTS.STATIC_IMAGES_PATH}more-benifits-world-icon.png`}
                     alt="globe"
                   />
-                  <h3>Largest Naming Community</h3>
-                  <p>
-                    Our unique approach allows you to receive an unmatched
-                    breadth of business name ideas from world's largest
-                    community of naming experts. With 75,000+ creatives and
-                    15,000+ successful naming projects, Squadhelp is by far the
-                    largest naming platform across the globe .
-                  </p>
+                  <h3>{mocks.largestNamingCommunity.title}</h3>
+                  <p>{mocks.largestNamingCommunity.text}</p>
                 </div>
                 <div className={styles.card}>
                   <img
                     src={`${CONSTANTS.STATIC_IMAGES_PATH}more-benifits-high-quality-icon.png`}
                     alt="desktop"
                   />
-                  <h3>High Quality & Collaboration</h3>
-                  <p>
-                    Using an advanced Quality Scoring Algorithm and Machine
-                    Learning, we ensure that you receive more ideas from our
-                    top-quality creatives, and Gamification best practices
-                    ensure two-way communication throughout your contest.
-                  </p>
+                  <h3>{mocks.highQualityCollaboration.title}</h3>
+                  <p>{mocks.highQualityCollaboration.text}</p>
                 </div>
                 <div className={styles.card}>
                   <img
                     src={`${CONSTANTS.STATIC_IMAGES_PATH}more-benifits-trademark-icon.png`}
                     alt="cards"
                   />
-                  <h3>Agency-Level Features</h3>
-                  <p>
-                    Squadhelp's high end Audience Testing service allows you to
-                    poll your target demographics to get unbiased feedback on
-                    your favorite names. Also receive Trademark support from our
-                    team of Licensed Trademark Attorneys, so you can pick your
-                    name with confidence.
-                  </p>
+                  <h3>{mocks.agencyLevelFeatures.title}</h3>
+                  <p>{mocks.agencyLevelFeatures.text}</p>
                 </div>
               </div>
             </div>
@@ -126,104 +104,42 @@ const Home = (props) => {
               </div>
               <div className={styles.stats}>
                 <div>
-                  <p>119,525</p>
-                  <span>Creatives</span>
+                  <p>{mocks.creatives.count}</p>
+                  <span>{mocks.creatives.title}</span>
                 </div>
                 <div>
-                  <p>21,875</p>
-                  <span>Customers</span>
+                  <p>{mocks.customers.count}</p>
+                  <span>{mocks.customers.title}</span>
                 </div>
                 <div>
-                  <p>85</p>
-                  <span>Industries</span>
+                  <p>{mocks.industries.count}</p>
+                  <span>{mocks.industries.title}</span>
                 </div>
               </div>
             </div>
-            <h2>How Do Name Contest Work?</h2>
-            <div className={styles.whiteContainer}>
-              <div className={styles.stepReverse}>
-                <div>
-                  <h3>Step 1: Launch a Naming Contest</h3>
-                  <p>
-                    <i className="fas fa-check" />
-                    <span>
-                      Start your project right with our proven Naming Brief
-                      template
-                    </span>
-                  </p>
-                  <p>
-                    <i className="fas fa-check" />
-                    <span>
-                      We’ll walk you through exactly what you need to share
-                      about your project in order to get an awesome Name
-                    </span>
-                  </p>
-                </div>
-                <img
-                  src={`${CONSTANTS.STATIC_IMAGES_PATH}gif/1-compressed.gif`}
-                  alt="compressed"
-                />
-              </div>
-            </div>
-            <div className={styles.greenContainer}>
-              <div className={styles.step}>
-                <img
-                  src={`${CONSTANTS.STATIC_IMAGES_PATH}gif/2-compressed-new.gif`}
-                  alt="compressed"
-                />
-                <div className={styles.greenStep}>
-                  <h3>Step 2: Ideas start pouring in within minutes</h3>
-                  <p>
-                    <i className="fas fa-check" />
-                    <span>
-                      100s of naming experts start submitting name ideas
-                    </span>
-                  </p>
-                  <p>
-                    <i className="fas fa-check" />
-                    <span>
-                      Names automatically checked for URL availability
-                    </span>
-                  </p>
+            <h2>{mocks.contestSteps.title}</h2>
+            {mocks.contestSteps.contestStepsItems.map((step) => (
+              <div className={styles[step.containerStyle]}>
+                <div className={styles[step.stepStyle]}>
+                  <img
+                    src={`${CONSTANTS.STATIC_IMAGES_PATH}gif/${step.stepImg}`}
+                    alt="compressed"
+                  />
+                  <div className={styles[step.stepSubStyle]}>
+                    <h3>{step.stepTitle}</h3>
+                    {step.texts.map((stepText) => (
+                      <p>
+                        <i className="fas fa-check" />
+                        <span>{stepText.text}</span>
+                      </p>
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className={styles.greyContainer}>
-              <div className={styles.stepReverse}>
-                <div>
-                  <h3>Step 3: Rate Entries & Brainstorm with Creatives</h3>
-                  <p>
-                    <i className="fas fa-check" />
-                    <span>Provide instant feedback on Names</span>
-                  </p>
-                  <p>
-                    <i className="fas fa-check" />
-                    <span>
-                      Send private feedback or public messages to all creatives
-                    </span>
-                  </p>
-                  <p>
-                    <i className="fas fa-check" />
-                    <span>
-                      The more entries you rate - the submissions get better and
-                      better
-                    </span>
-                  </p>
-                </div>
-                <img
-                  src={`${CONSTANTS.STATIC_IMAGES_PATH}gif/3-compressed.gif`}
-                  alt="compressed"
-                />
-              </div>
-            </div>
+            ))}
             <div className={styles.headerBar}>
-              <h3>Names For Sale</h3>
-              <p className={styles.blueUnderline}>
-                Not interested in launching a contest? Purchase a name instantly
-                from our hand-picked collection of premium names. Price includes
-                a complimentary Trademark Report, a Domain name as well as a
-                Logo design
-              </p>
+              <h3>{mocks.namesForSale.title}</h3>
+              <p className={styles.blueUnderline}>{mocks.namesForSale.text}</p>
             </div>
             <SlideBar
               images={carouselConstants.exampleSliderImages}
