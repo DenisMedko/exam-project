dc84d8dd9899b0a8dd76e61159ddb6b0c15f242f
DenisMedko <medko.denis76@gmail.com>
2023-03-08 19:28:56 +0200

    RegistrationPage to hooks refactored

--- client/src/pages/RegistrationPage/RegistrationPage.jsx
+++ client/src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,12 +1,17 @@
 import React, { Fragment } from 'react';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { bindActionCreators } from '@reduxjs/toolkit';
+import * as authActionCreators from '../../store/slices/authSlice';
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
 import styles from './RegistrationPage.module.sass';
-import { clearAuthError } from '../../store/slices/authSlice';
 import HeaderSignUp from '../../components/HeaderSignUp/HeaderSignUp';
 import CONSTANTS from '../../constants';
 
-const RegistrationPage = (props) => {
+const RegistrationPage = ({ history }) => {
+  const { clearAuthError: clearError } = bindActionCreators(
+    { ...authActionCreators },
+    useDispatch()
+  );
   const renderArticles = (column) => {
     return column.articles.map((article) => (
       <Fragment key={article.id}>
@@ -28,8 +33,12 @@ const RegistrationPage = (props) => {
   return (
     <div className={styles.signUpPage}>
       <div className={styles.signUpContainer}>
-        <HeaderSignUp {...props} page={'login'} />
-        <RegistrationForm history={props.history} />
+        <HeaderSignUp
+          history={history}
+          clearError={clearError}
+          page={'login'}
+        />
+        <RegistrationForm history={history} />
       </div>
       <div className={styles.footer}>
         <div className={styles.articlesMainContainer}>
@@ -40,8 +49,4 @@ const RegistrationPage = (props) => {
   );
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  clearError: () => dispatch(clearAuthError()),
-});
-
-export default connect(null, mapDispatchToProps)(RegistrationPage);
+export default RegistrationPage;
