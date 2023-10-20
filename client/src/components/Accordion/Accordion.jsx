64141919e4bdec5823c6bd1a139a0bb3b7988292
DenisMedko <medko.denis76@gmail.com>
2023-10-20 13:38:25 +0300

    layout scrolling correction

--- client/src/components/Accordion/Accordion.jsx
+++ client/src/components/Accordion/Accordion.jsx
@@ -3,36 +3,51 @@ import classNames from 'classnames';
 import styles from './Accordion.module.sass';
 import CONSTANTS from '../../constants';
 
-const Accordion = (props) => {
-  const { header, text } = props;
-  const [isActive, setIsActive] = useState(false);
-  return (
-    <>
-      <button
-        onClick={() => {
-          setIsActive(!isActive);
-        }}
-        className={styles.faqBtn}
-      >
-        {header}
-        <div className={styles.arrowBtn}>
-          <img
-            className={classNames({
-              [styles.arrowPicRotate]: isActive,
-              [styles.arrowPicRotateBack]: !isActive,
-            })}
-            src={`${CONSTANTS.STATIC_IMAGES_PATH}arrow.svg`}
-            alt="arrow"
+const Accordion = ({ items }) => {
+  const [activeItemIndex, setActiveItemIndex] = useState(0);
+
+  const onClickHandler = (itemIndex) => {
+    activeItemIndex !== itemIndex
+      ? setActiveItemIndex(itemIndex)
+      : setActiveItemIndex(null);
+  };
+
+  const renderItem = (item, activeItemIndex, itemIndex) => {
+    const { id, header, text } = item;
+    const isActive = activeItemIndex === itemIndex;
+    return (
+      <div key={id} className={styles.item}>
+        <button
+          onClick={() => onClickHandler(itemIndex)}
+          className={styles.itemBtn}
+        >
+          {header}
+          <div className={styles.arrowBtn}>
+            <img
+              className={classNames({
+                [styles.arrowPicRotate]: isActive,
+                [styles.arrowPicRotateBack]: !isActive,
+              })}
+              src={`${CONSTANTS.STATIC_IMAGES_PATH}arrow.svg`}
+              alt="arrow"
+            />
+          </div>
+        </button>
+        {isActive && (
+          <div
+            className={styles.itemText}
+            dangerouslySetInnerHTML={{ __html: text }}
           />
-        </div>
-      </button>
-      {isActive && (
-        <div
-          className={styles.faqText}
-          dangerouslySetInnerHTML={{ __html: text }}
-        />
+        )}
+      </div>
+    );
+  };
+  return (
+    <article>
+      {items.map((item, itemIndex) =>
+        renderItem(item, activeItemIndex, itemIndex)
       )}
-    </>
+    </article>
   );
 };
 
--- client/src/pages/HowItWorksPage/FAQTopicsSection/FAQTopicsSection.jsx
+++ client/src/pages/HowItWorksPage/FAQTopicsSection/FAQTopicsSection.jsx
@@ -1,40 +1,50 @@
+import React, { useState, useEffect } from 'react';
 import classNames from 'classnames';
 import Accordion from '../../../components/Accordion/Accordion';
 import pageStyles from '../HowItWorksPage.module.sass';
 import styles from './FAQTopicsSection.module.sass';
 
 const FAQTopicsSection = ({ content }) => {
-  const renderTopics = ({ faqs }) => {
-    return (
-      <article>
-        {faqs.map((faq) => (
-          <div key={faq.id} className={styles.faq}>
-            <Accordion header={faq.header} text={faq.text} />
-          </div>
-        ))}
-      </article>
-    );
+  const [refsArray, setRefsArray] = useState([]);
+
+  useEffect(() => {
+    setRefsArray(content.map(() => React.createRef()));
+  }, [content]);
+
+  const onClickHandler = (topicIndex) => {
+    if (refsArray[topicIndex].current) {
+      refsArray[topicIndex].current.scrollIntoView({
+        behavior: 'smooth',
+      });
+    }
   };
+
+  const renderTopicsList = () => {
+    return content.map((topic, topicIndex) => (
+      <li key={topic.id}>
+        <div className={styles.link} onClick={() => onClickHandler(topicIndex)}>
+          {topic.title}
+        </div>
+      </li>
+    ));
+  };
+
+  const renderAccordion = () => {
+    return content.map((topic, topicIndex) => {
+      return (
+        <div key={topic.id} className={styles.topic}>
+          <hr ref={refsArray[topicIndex]} />
+          <h3 className={styles.topicTitle}>{topic.title}</h3>
+          <Accordion key={topic.id} items={topic.faqs} />
+        </div>
+      );
+    });
+  };
+
   return (
     <div className={classNames(pageStyles.container, styles.topicsContainer)}>
-      <ul className={styles.topicsList}>
-        {content.map((topic) => (
-          <li key={topic.id}>
-            <a className={styles.link} href={`#${topic.url}`}>
-              {topic.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <div className={styles.topics}>
-        {content.map((topic, i) => (
-          <div key={topic.id} id={topic.url} className={styles.topic}>
-            <h3 className={styles.topicTitle}>{topic.title}</h3>
-            {renderTopics(topic)}
-            {content.length - 1 !== i && <hr />}
-          </div>
-        ))}
-      </div>
+      <ul className={styles.topicsList}>{renderTopicsList()}</ul>
+      <div className={styles.topics}>{renderAccordion()}</div>
     </div>
   );
 };
